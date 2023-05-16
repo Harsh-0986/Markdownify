8bcc10fe642d5da5a470bbf6f7c3c7c0f7792cce
Harsh-0986 <harsh2102.dev@gmail.com>
2023-05-16 09:46:45 +0000

    Deploy to vercel

--- src/app/view/[id]/page.tsx
+++ src/app/view/[id]/page.tsx
@@ -2,6 +2,7 @@
 import { RawNote } from "@/app/page";
 import { Title } from "@tremor/react";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import remarkGfm from "remark-gfm";
